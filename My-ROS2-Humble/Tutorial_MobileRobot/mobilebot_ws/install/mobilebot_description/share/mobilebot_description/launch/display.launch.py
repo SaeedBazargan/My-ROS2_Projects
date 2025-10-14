cade1b878d4dbf2c540d177125e39b60f9c40815
SaeedBazargan <bazargan0241private@gmail.com>
2025-10-14 16:19:15 +0330

    The URDF file for tutorial_mobilebot is completed and now its ready for simulation.

--- My-ROS2-Humble/Tutorial_MobileRobot/mobilebot_ws/install/mobilebot_description/share/mobilebot_description/launch/display.launch.py
+++ My-ROS2-Humble/Tutorial_MobileRobot/mobilebot_ws/install/mobilebot_description/share/mobilebot_description/launch/display.launch.py
@@ -8,7 +8,7 @@ from ament_index_python.packages import get_package_share_path
 
 def generate_launch_description():
     urdf_path = os.path.join(get_package_share_path('mobilebot_description'),
-                             'urdf', 'mobilebot.urdf')
+                             'urdf', 'mobilebot.urdf.xacro')
     rviz_config_path = os.path.join(get_package_share_path('mobilebot_description'),
                              'rviz', 'mobilebot_config.rviz')
 
