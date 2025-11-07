b3b741020cb58a5f9f87c586601488c5469893cd
SaeedBazargan <bazargan0241private@gmail.com>
2025-11-07 22:53:26 +0330

    Launch file modified.

--- My-ROS2-Humble/Kukabot_MobileRobot/kukabot_ws/src/kukabot_bringup/launch/kukabot_sim.launch.py
+++ My-ROS2-Humble/Kukabot_MobileRobot/kukabot_ws/src/kukabot_bringup/launch/kukabot_sim.launch.py
@@ -1,117 +1,108 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler, SetEnvironmentVariable
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
 import os
 import xacro
 from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
-from launch.actions import RegisterEventHandler, SetEnvironmentVariable
-from launch.event_handlers import OnProcessExit
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
-
 
 def generate_launch_description():
-    # Launch Arguments
-    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+    # Paths
+    description_pkg_path = get_package_share_directory("kukabot_description")
+    bringup_pkg_path = get_package_share_directory("kukabot_bringup")
 
-    kukabot_description_path = os.path.join(get_package_share_directory('kukabot_description'))
-    kukabot_sim_path = os.path.join(get_package_share_directory('kukabot_bringup'))
-    
-    # Set gazebo sim resource path
+    urdf_path = os.path.join(description_pkg_path, "urdf", "main.urdf.xacro")
+    rviz_config_file = os.path.join(description_pkg_path, "rviz", "rviz_config.rviz")
+
+    # URDF
+    robot_description = xacro.process_file(urdf_path, mappings={"use_sim": "true"}).toxml()
+
+    # Publish TF + robot model
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description}],
+        output="screen",
+    )
+
+    # Make Gazebo find models
     gazebo_resource_path = SetEnvironmentVariable(
-        name='GZ_SIM_RESOURCE_PATH',
-        value=[os.path.join(kukabot_sim_path, 'worlds'), ':' + str(Path(kukabot_description_path).parent.resolve())]
+        name="GZ_SIM_RESOURCE_PATH",
+        value=[os.path.join(bringup_pkg_path, "worlds"), ":" + str(Path(description_pkg_path).parent.resolve())]
     )
 
-    arguments = LaunchDescription([DeclareLaunchArgument('world', default_value='kukabot_world', description='Gz sim World'),])
-
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
-                launch_arguments=[('gz_args', [LaunchConfiguration('world'), '.sdf', ' -v 4', ' -r'])]
+    # World argument
+    declare_world_arg = DeclareLaunchArgument(
+        "world",
+        default_value="kukabot_world",
     )
 
-    xacro_file = os.path.join(kukabot_description_path, 'urdf', 'main.urdf.xacro')
-
-    doc = xacro.process_file(xacro_file, mappings={'use_sim' : 'true'})
-
-    robot_desc = doc.toprettyxml(indent='  ')
-
-    params = {'robot_description': robot_desc}
-
-    node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[params]
+    # Start Gazebo
+    start_gazebo = IncludeLaunchDescription(PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory("ros_gz_sim"), "launch", "gz_sim.launch.py")),
+            launch_arguments={"gz_args": [LaunchConfiguration("world"), ".sdf", " -v 4", " -r"]}.items()
     )
 
-    gz_spawn_entity = Node(
-        package='ros_gz_sim',
-        executable='create',
-        output='screen',
-        arguments=['-string', robot_desc,
-                   '-x', '0.0',
-                   '-y', '0.0',
-                   '-z', '0.07',
-                   '-R', '0.0',
-                   '-P', '0.0',
-                   '-Y', '0.0',
-                   '-name', 'fws_robot',
-                   '-allow_renaming', 'false'],
-    )
-
-    load_joint_state_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
-        output='screen'
-    )
-
-    load_forward_velocity_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'forward_velocity_controller'],
-        output='screen'
+    # Spawn robot into Gazebo
+    gz_spawn_entity_node = Node(
+        package="ros_gz_sim",
+        executable="create",
+        arguments=[
+            "-string", robot_description,
+            "-name", "kukabot",
+            "-allow_renaming", "false",
+            "-x", "0", 
+            "-y", "0", 
+            "-z", "0.07"],
+        output="screen",
     )
 
     # Bridge
-    bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=['/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'],
-        output='screen'
+    ros_gazebo_bridge_node = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        output="screen",
     )
 
-    rviz_config_file = os.path.join(kukabot_description_path, 'rviz', 'rviz_config.rviz')
+    # Controller Spawners (correct & clean)
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+        output="screen",
+    )
 
-    rviz = Node(
+    forward_velocity_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["forward_velocity_controller"],
+        output="screen",
+    )
+
+    # RViz
+    rviz_node = Node(
         package="rviz2",
         executable="rviz2",
         name="rviz2",
         output="log",
-        arguments=["-d", rviz_config_file],
+        arguments=["-d", rviz_config_file]
     )
 
+    # Ensure correct order: Spawn robot → load joint_state → load drive controller
     return LaunchDescription([
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=gz_spawn_entity,
-                on_exit=[load_joint_state_controller],
-            )
-        ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-               target_action=load_joint_state_controller,
-               on_exit=[load_forward_velocity_controller],
-            )
-        ),
         gazebo_resource_path,
-        arguments,
-        gazebo,
-        node_robot_state_publisher,
-        gz_spawn_entity,
-        bridge,
-        rviz,
-    ])
+        declare_world_arg,
+        start_gazebo,
+        robot_state_publisher,
+        gz_spawn_entity_node,
+        ros_gazebo_bridge_node,
+        rviz_node,
 
+        RegisterEventHandler(OnProcessExit(target_action=gz_spawn_entity_node, on_exit=[joint_state_broadcaster_spawner])),
+        RegisterEventHandler(OnProcessExit(target_action=joint_state_broadcaster_spawner, on_exit=[forward_velocity_controller_spawner])),
+    ])
