439e043ef9b511ba3259ee40217cbb69d6cecb56
SaeedBazargan <31041761+SaeedBazargan@users.noreply.github.com>
2026-07-20 16:50:22 +0330

    Delete My-ROS2/Tutorial_Nav2_Turtlebot3/turtlebot3_ws/build directory

--- My-ROS2/Tutorial_Nav2_Turtlebot3/turtlebot3_ws/build/turtlebot3_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/saeedbzrgn/Desktop/My-ROS2_Projects/My-ROS2-Humble/Tutorial_Nav2_Turtlebot3/turtlebot3_ws/src/turtlebot3_gazebo
-# Build directory: /home/saeedbzrgn/Desktop/My-ROS2_Projects/My-ROS2-Humble/Tutorial_Nav2_Turtlebot3/turtlebot3_ws/build/turtlebot3_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- My-ROS2/Tutorial_Nav2_Turtlebot3/turtlebot3_ws/build/turtlebot3_simulations/colcon_command_prefix_build.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/saeedbzrgn/Desktop/My-ROS2_Projects/My-ROS2-Humble/Tutorial_Nav2_Turtlebot3/turtlebot3_ws/install/turtlebot3_fake_node/share/turtlebot3_fake_node/package.sh"
-. "/home/saeedbzrgn/Desktop/My-ROS2_Projects/My-ROS2-Humble/Tutorial_Nav2_Turtlebot3/turtlebot3_ws/install/turtlebot3_gazebo/share/turtlebot3_gazebo/package.sh"
-. "/home/saeedbzrgn/Desktop/My-ROS2_Projects/My-ROS2-Humble/Tutorial_Nav2_Turtlebot3/turtlebot3_ws/install/turtlebot3_manipulation_gazebo/share/turtlebot3_manipulation_gazebo/package.sh"
